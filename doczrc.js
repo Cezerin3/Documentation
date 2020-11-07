6b5afe9aad4a7f2348d2c94ca999c258cca8e3cf
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-11-07 15:58:21 +0530

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,8 @@
 export default {
+  title: "Cezerin3",
+  description: "Cezerin3 Documentation and community site.",
+  src: "./src/docs",
+  editBranch: "main",
   themeConfig: {
     mode: "dark",
   },
