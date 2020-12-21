95a66d1c5aa4ba27d473e9ab476d8729a61c5550
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-12-21 14:39:12 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,14 @@
 import { graphql, Link, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 import {
+  MDBCol,
   MDBCollapse,
   MDBNavbar,
   MDBNavbarBrand,
   MDBNavbarNav,
   MDBNavbarToggler,
   MDBNavItem,
+  MDBRow,
 } from "mdbreact"
 import React, { useState } from "react"
 
@@ -31,21 +33,34 @@ const NavBar = () => {
   return (
     <MDBNavbar color="blue" dark expand="md">
       <MDBNavbarBrand>
-        <Img fluid={data.logo.childImageSharp.fluid} />
-        <strong className="white-text">
-          <Link to="/" style={{ color: "white" }}>
-            Cezerin3
-          </Link>
-        </strong>
+        <MDBRow>
+          <MDBCol>
+            <Img
+              fluid={data.logo.childImageSharp.fluid}
+              alt="logo"
+              className="w-50 p-3"
+            />
+          </MDBCol>
+          <MDBCol>
+            <strong className="white-text">
+              <Link to="/" style={{ color: "white" }}>
+                Cezerin3
+              </Link>
+            </strong>
+          </MDBCol>
+        </MDBRow>
       </MDBNavbarBrand>
       <MDBNavbarToggler onClick={toggleCollapse} />
       <MDBCollapse id="navbarCollapse3" isOpen={isOpen} navbar>
         <MDBNavbarNav left></MDBNavbarNav>
         <MDBNavbarNav right>
           <MDBNavItem>
-            <Link to="/docs" className="white-text nav-link">
+            <a
+              href="https://github.com/Cezerin3/Store"
+              className="white-text nav-link"
+            >
               GitHub
-            </Link>
+            </a>
           </MDBNavItem>
           <MDBNavItem>
             <Link to="/docs" className="white-text nav-link">
